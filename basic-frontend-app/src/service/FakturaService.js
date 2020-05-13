6f2e0180f1f42304e86d9e7e5ae19964d34b4243
Tomáš Vacek <55704922+vacek69@users.noreply.github.com>
2020-05-13 16:02:47 +0200

    Create fakturaService

---
+++ basic-frontend-app/src/service/FakturaService.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import AuthService from './AuthService';
+import config from '../infrastucture/config';
+import {FakturaController_delete, FakturaController_getById, FakturaController_listFaktura, FakturaController_save, FakturaController_update} from '../infrastucture/ApiRoutes';
+
+
+class FakturaSerive {
+
+    fetchFaktury(pageNo = 0, pageSize = 10, sortAsc = true, sortBy ="evidencniCislo") {
+        return axios.get(config.API_BASE_URL + FakturaController_listFaktura(pageNo,pageSize, sortAsc, sortBy), AuthService.getAuthHeader());
+    }
+
+    fetchFakturaById(id) {
+        return axios.get(config.API_BASE_URL + FakturaController_getById(id), AuthService.getAuthHeader());
+    }
+
+    deleteFaktura(id) {
+        return axios.delete(config.API_BASE_URL + FakturaController_delete(id), AuthService.getAuthHeader());
+    }
+
+    addFaktura(faktura) {
+        return axios.post(config.API_BASE_URL + FakturaController_save, faktura, AuthService.getAuthHeader());
+    }
+
+    editFaktura(id, faktura) {
+        return axios.put(config.API_BASE_URL + FakturaController_update(user.id), faktura, AuthService.getAuthHeader());
+    }
+
+}
+
+export default new FakturaSerive();
